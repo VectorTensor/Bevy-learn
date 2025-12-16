2248f39a9311fcea21c8553059e1c873a2b1b62d
VectorTensor <prayashthapa15@gmail.com>
2025-12-16 07:12:15 +0545

    snake updated movement and positions

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,37 @@ use bevy::prelude::*;
 #[derive(Component)]
 struct SnakeHead;
 
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
+
 const SNAKE_HEAD_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
+const ARENA_WIDTH: u32 = 100;
+const ARENA_HEIGHT: u32 = 100;
 
 fn main() {
     App::new()
         .add_systems(Startup, setup_camera)
         .add_systems(Startup, spawn_snake)
         .add_systems(Update, snake_movement)
+        .add_systems(Update, (position_translation, size_scaling))
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -19,38 +43,74 @@ fn setup_camera(mut commands: Commands) {
 }
 
 fn spawn_snake(mut commands: Commands) {
-    commands.spawn((
-        Sprite {
-            color: SNAKE_HEAD_COLOR,
-            custom_size: Some(Vec2::splat(10.0)),
-            ..default()
-        },
-        Transform::default(),
-        GlobalTransform::default(),
-        Visibility::Visible,
-        SnakeHead,
-    ));
+    commands
+        .spawn((
+            Sprite {
+                color: SNAKE_HEAD_COLOR,
+                custom_size: Some(Vec2::splat(10.0)),
+                ..default()
+            },
+            Transform::default(),
+            GlobalTransform::default(),
+            Visibility::Visible,
+        ))
+        .insert(SnakeHead)
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8));
 }
 
 fn snake_movement(
     keyboard: Res<ButtonInput<KeyCode>>,
-    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
+    mut head_positions: Query<&mut Position, With<SnakeHead>>,
 ) {
-    for mut transform in &mut head_positions {
+    for mut pos in &mut head_positions {
         if keyboard.pressed(KeyCode::ArrowLeft) {
-            transform.translation.x -= 2.0;
+            pos.x -= 1;
         }
 
         if keyboard.pressed(KeyCode::ArrowRight) {
-            transform.translation.x += 2.0;
+            pos.x += 1;
         }
 
         if keyboard.pressed(KeyCode::ArrowDown) {
-            transform.translation.y -= 2.0;
+            pos.y -= 1;
         }
 
         if keyboard.pressed(KeyCode::ArrowUp) {
-            transform.translation.y += 2.0;
+            pos.y += 1;
         }
+    }
+}
+
+fn size_scaling(windows: Query<&Window>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.iter().next().unwrap();
+
+    for (sprite_size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width(),
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height(),
+            1.0,
+        );
+    }
+}
+
+fn position_translation(
+    windows: Query<&Window, With<bevy::window::PrimaryWindow>>,
+    mut q: Query<(&Position, &mut Transform)>,
+) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.0) + (tile_size / 2.0)
+    }
+
+    let window = windows.single(); // There should only be one primary window
+    let window = window.unwrap();
+
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width(), ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height(), ARENA_HEIGHT as f32),
+            0.0,
+        );
     }
 }
