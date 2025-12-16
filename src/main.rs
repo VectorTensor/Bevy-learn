a78b0b9e2d2b3eb8d73d7d80f9e7eeaa1c4eda47
VectorTensor <prayashthapa15@gmail.com>
2025-12-16 06:01:47 +0545

    basic rendering and movement

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,56 @@
 use bevy::prelude::*;
 
-#[cfg(not(target_arch = "wasm32"))]
-use bevy::sprite_render::{Wireframe2dConfig, Wireframe2dPlugin};
+#[derive(Component)]
+struct SnakeHead;
+
+const SNAKE_HEAD_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
 
 fn main() {
-    let mut app = App::new();
-    app.add_plugins((
-        DefaultPlugins,
-        #[cfg(not(target_arch = "wasm32"))]
-        Wireframe2dPlugin::default(),
-    ))
-    .add_systems(Startup, setup);
-    #[cfg(not(target_arch = "wasm32"))]
-    app.add_systems(Update, toggle_wireframe);
-    app.run();
+    App::new()
+        .add_systems(Startup, setup_camera)
+        .add_systems(Startup, spawn_snake)
+        .add_systems(Update, snake_movement)
+        .add_plugins(DefaultPlugins)
+        .run();
 }
 
-const X_EXTENT: f32 = 900.;
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
 
-    let shapes = [
-        meshes.add(Circle::new(50.0)),
-        meshes.add(CircularSector::new(50.0, 1.0)),
-        meshes.add(CircularSegment::new(50.0, 1.25)),
-        meshes.add(Ellipse::new(25.0, 50.0)),
-        meshes.add(Annulus::new(25.0, 50.0)),
-        meshes.add(Capsule2d::new(25.0, 50.0)),
-        meshes.add(Rhombus::new(75.0, 100.0)),
-        meshes.add(Rectangle::new(50.0, 100.0)),
-        meshes.add(RegularPolygon::new(50.0, 6)),
-        meshes.add(Triangle2d::new(
-            Vec2::Y * 50.0,
-            Vec2::new(-50.0, -50.0),
-            Vec2::new(50.0, -50.0),
-        )),
-        meshes.add(Segment2d::new(
-            Vec2::new(-50.0, 50.0),
-            Vec2::new(50.0, -50.0),
-        )),
-        meshes.add(Polyline2d::new(vec![
-            Vec2::new(-50.0, 50.0),
-            Vec2::new(0.0, -50.0),
-            Vec2::new(50.0, 50.0),
-        ])),
-    ];
-
-    let num_shapes = shapes.len();
-
-    for (i, shape) in shapes.into_iter().enumerate() {
-        let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
-        commands.spawn((
-            Mesh2d(shape),
-            MeshMaterial2d(materials.add(color)),
-            Transform::from_xyz(
-                X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * X_EXTENT,
-                0.0,
-                0.0,
-            ),
-        ));
-    }
-    #[cfg(not(target_arch = "wasm32"))]
+fn spawn_snake(mut commands: Commands) {
     commands.spawn((
-        Text::new("Press space to toggle wireframes"),
-        Node {
-            position_type: PositionType::Absolute,
-            top: px(12),
-            left: px(12),
+        Sprite {
+            color: SNAKE_HEAD_COLOR,
+            custom_size: Some(Vec2::splat(10.0)),
             ..default()
         },
+        Transform::default(),
+        GlobalTransform::default(),
+        Visibility::Visible,
+        SnakeHead,
     ));
 }
 
-#[cfg(not(target_arch = "wasm32"))]
-fn toggle_wireframe(
-    mut wireframe_config: ResMut<Wireframe2dConfig>,
+fn snake_movement(
     keyboard: Res<ButtonInput<KeyCode>>,
+    mut head_positions: Query<&mut Transform, With<SnakeHead>>,
 ) {
-    if keyboard.just_pressed(KeyCode::Space) {
-        wireframe_config.global = !wireframe_config.global;
+    for mut transform in &mut head_positions {
+        if keyboard.pressed(KeyCode::ArrowLeft) {
+            transform.translation.x -= 2.0;
+        }
+
+        if keyboard.pressed(KeyCode::ArrowRight) {
+            transform.translation.x += 2.0;
+        }
+
+        if keyboard.pressed(KeyCode::ArrowDown) {
+            transform.translation.y -= 2.0;
+        }
+
+        if keyboard.pressed(KeyCode::ArrowUp) {
+            transform.translation.y += 2.0;
+        }
     }
 }
