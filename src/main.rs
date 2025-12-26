6dba46b80a25d52367f29901d7d13d9170c7bf4a
VectorTensor <prayashthapa15@gmail.com>
2025-12-26 20:45:10 +0545

    feat:: basic 3d scene

--- src/main.rs
+++ src/main.rs
@@ -1,209 +1,42 @@
-use bevy::{
-    core_pipeline::tonemapping::{DebandDither, Tonemapping},
-    input::keyboard::Key,
-    post_process::bloom::{Bloom, BloomCompositeMode},
-    prelude::*,
-};
+use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, update_bloom_settings)
         .run();
 }
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    //Circular base
     commands.spawn((
-        Camera2d,
-        Camera {
-            clear_color: ClearColorConfig::Custom(Color::BLACK),
+        Mesh3d(meshes.add(Circle::new(4.0))),
+        MeshMaterial3d(materials.add(Color::WHITE)),
+        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+    ));
+
+    //cube
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(225, 144, 255))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
+
+    // light
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
             ..default()
         },
-        Tonemapping::TonyMcMapface,
-        Bloom::default(),
-        DebandDither::Enabled,
-    ));
-    // Circle mesh
-    commands.spawn((
-        Mesh2d(meshes.add(Circle::new(100.))),
-        // 3. Put something bright in a dark environment to see the effect
-        MeshMaterial2d(materials.add(Color::srgb(7.5, 0.0, 7.5))),
-        Transform::from_translation(Vec3::new(-200., 0., 0.)),
-    ));
-
-    commands.spawn(Sprite {
-        image: asset_server.load("branding/bevy_bird_dark.png"),
-        color: Color::srgb(5.0, 5.0, 5.0),
-        custom_size: Some(Vec2::splat(160.0)),
-        ..default()
-    });
-
-    commands.spawn((
-        Mesh2d(meshes.add(RegularPolygon::new(100., 6))),
-        MeshMaterial2d(materials.add(Color::srgb(6.25, 9.4, 9.1))),
-        Transform::from_translation(Vec3::new(200., 0., 0.)),
+        Transform::from_xyz(4.0, 8.0, 4.0),
     ));
 
     commands.spawn((
-        Text::default(),
-        Node {
-            position_type: PositionType::Absolute,
-            top: px(12),
-            left: px(12),
-            ..default()
-        },
+        Camera3d::default(),
+        Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
-}
-
-fn update_bloom_settings(
-    camera: Single<(Entity, &Tonemapping, Option<&mut Bloom>), With<Camera>>,
-    mut text: Single<&mut Text>,
-    mut commands: Commands,
-    keycode: Res<ButtonInput<KeyCode>>,
-    time: Res<Time>,
-) {
-    let (camera_entity, tonemapping, bloom) = camera.into_inner();
-
-    match bloom {
-        Some(mut bloom) => {
-            text.0 = "Bloom (Toggle: Space)\n".to_string();
-            text.push_str(&format!("(Q/A) Intensity: {:.2} \n", bloom.intensity));
-            text.push_str(&format!(
-                "(W/S) Low-frequency boost curvature: {:.2}\n",
-                bloom.low_frequency_boost_curvature
-            ));
-            text.push_str(&format!(
-                "(T/G) High-pass frequency : {:.2}\n",
-                bloom.high_pass_frequency
-            ));
-            text.push_str(&format!(
-                "(T/G) Mode {}\n",
-                match bloom.composite_mode {
-                    BloomCompositeMode::EnergyConserving => "Energy-conserving",
-                    BloomCompositeMode::Additive => "Additive",
-                }
-            ));
-            text.push_str(&format!(
-                "(Y/H) Threshold: {:.2}\n",
-                bloom.prefilter.threshold
-            ));
-            text.push_str(&format!(
-                "(U/J) Threshold softness: {:.2}\n",
-                bloom.prefilter.threshold_softness
-            ));
-            text.push_str(&format!("(I/K) Horizontal Scale: {:.2} \n", bloom.scale.x));
-
-            if keycode.just_pressed(KeyCode::Space) {
-                commands.entity(camera_entity).remove::<Bloom>();
-            }
-
-            let dt = time.delta_secs();
-
-            if keycode.pressed(KeyCode::KeyA) {
-                bloom.intensity -= dt / 10.0;
-            }
-
-            if keycode.pressed(KeyCode::KeyQ) {
-                bloom.intensity += dt / 10.0;
-            }
-            bloom.intensity = bloom.intensity.clamp(0.0, 1.0);
-
-            if keycode.pressed(KeyCode::KeyS) {
-                bloom.low_frequency_boost -= dt / 10.0;
-            }
-
-            if keycode.pressed(KeyCode::KeyD) {
-                bloom.low_frequency_boost_curvature -= dt / 10.0;
-            }
-
-            if keycode.pressed(KeyCode::KeyE) {
-                bloom.low_frequency_boost_curvature += dt / 10.0;
-            }
-
-            bloom.low_frequency_boost_curvature =
-                bloom.low_frequency_boost_curvature.clamp(0.0, 1.0);
-
-            if keycode.pressed(KeyCode::KeyF) {
-                bloom.high_pass_frequency -= dt / 10.0;
-            }
-
-            if keycode.pressed(KeyCode::KeyR) {
-                bloom.high_pass_frequency += dt / 10.0;
-            }
-
-            bloom.high_pass_frequency = bloom.high_pass_frequency.clamp(0.0, 1.0);
-
-            if keycode.pressed(KeyCode::KeyG) {
-                bloom.composite_mode = BloomCompositeMode::Additive;
-            }
-
-            if keycode.pressed(KeyCode::KeyT) {
-                bloom.composite_mode = BloomCompositeMode::EnergyConserving;
-            }
-
-            if keycode.pressed(KeyCode::KeyH) {
-                bloom.prefilter.threshold -= dt;
-            }
-
-            if keycode.pressed(KeyCode::KeyY) {
-                bloom.prefilter.threshold += dt;
-            }
-
-            bloom.prefilter.threshold = bloom.prefilter.threshold.max(0.0);
-
-            if keycode.pressed(KeyCode::KeyJ) {
-                bloom.prefilter.threshold_softness -= dt / 10.0;
-            }
-
-            if keycode.pressed(KeyCode::KeyU) {
-                bloom.prefilter.threshold_softness -= dt / 10.0;
-            }
-
-            bloom.prefilter.threshold_softness = bloom.prefilter.threshold_softness.clamp(0.0, 1.0);
-
-            if keycode.pressed(KeyCode::KeyK) {
-                bloom.scale.x -= dt * 2.0;
-            }
-
-            if keycode.pressed(KeyCode::KeyI) {
-                bloom.scale.x += dt * 2.0;
-            }
-            bloom.scale.x = bloom.scale.x.clamp(0.0, 16.0);
-        }
-
-        None => {
-            text.0 = "Bloom: Off (Toggle: Space) \n".to_string();
-
-            if keycode.just_pressed(KeyCode::Space) {
-                commands.entity(camera_entity).insert(Bloom::default());
-            }
-        }
-    }
-
-    text.push_str(&format!("(O) Tonemapping: {tonemapping:?} \n"));
-
-    if keycode.just_pressed(KeyCode::KeyO) {
-        commands
-            .entity(camera_entity)
-            .insert(next_tonemap(tonemapping));
-    }
-}
-/// Get the next Tonemapping algorithm
-fn next_tonemap(tonemapping: &Tonemapping) -> Tonemapping {
-    match tonemapping {
-        Tonemapping::None => Tonemapping::AcesFitted,
-        Tonemapping::AcesFitted => Tonemapping::AgX,
-        Tonemapping::AgX => Tonemapping::BlenderFilmic,
-        Tonemapping::BlenderFilmic => Tonemapping::Reinhard,
-        Tonemapping::Reinhard => Tonemapping::ReinhardLuminance,
-        Tonemapping::ReinhardLuminance => Tonemapping::SomewhatBoringDisplayTransform,
-        Tonemapping::SomewhatBoringDisplayTransform => Tonemapping::TonyMcMapface,
-        Tonemapping::TonyMcMapface => Tonemapping::None,
-    }
 }
